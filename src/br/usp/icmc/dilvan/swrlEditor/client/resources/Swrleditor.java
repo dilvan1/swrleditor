4e742e32fd3f0517371ecafac02036c7d7b90314
jel phd <jel.phd@gmail.com>
2011-10-03 10:28:13 -0300

    Modifies how to use CSS

---
+++ src/br/usp/icmc/dilvan/swrlEditor/client/resources/Swrleditor.java
@@ -0,0 +1,104 @@
+package br.usp.icmc.dilvan.swrlEditor.client.resources;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface Swrleditor extends CssResource {
+
+	String panel();
+
+	@ClassName("pointer-cursor")
+	String pointerCursor();
+
+	String separador();
+
+	String itemFilter();
+
+	String editor_IVARIABLE();
+
+	@ClassName("white-background")
+	String whiteBackground();
+
+	String swrltext();
+
+	String det();
+
+	String q_builtin();
+
+	String link();
+
+	String atom_BUILTIN();
+
+	@ClassName("swrl-rule")
+	String swrlRule();
+
+	String noerror();
+
+	@ClassName("link-remove")
+	String linkRemove();
+
+	String border_panel();
+
+	@ClassName("decision-tree-background")
+	String decisionTreeBackground();
+
+	String param_INDIVIDUALID();
+
+	String linkselected();
+
+	@ClassName("popup-title")
+	String popupTitle();
+
+	String mlink();
+
+	String param_IVARIABLE();
+
+	String treeItemLink();
+
+	String atom_SAME_DIFERENT();
+
+	String error();
+
+	String param_DVARIABLE();
+
+	String vtlink();
+
+	String q_subnivel();
+
+	String editor_DVARIABLE();
+
+	@ClassName("header-name-class")
+	String headerNameClass();
+
+	String label();
+
+	String hint();
+
+	String treeEntity();
+
+	String properties();
+
+	String atom_INDIVIDUAL_PROPERTY();
+
+	String peq();
+
+	@ClassName("gwt-TreeItem")
+	String gwtTreeItem();
+
+	String q_class();
+	
+	
+	String antecedent();
+	
+	String consequent();
+
+	String atom_CLASS();
+
+	String atom_DATAVALUE_PROPERTY();
+	
+	String atom_DATA_RANGE();
+
+	String warning();
+
+	String param_DATALITERAL();
+
+}
